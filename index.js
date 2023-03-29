51c4cfda4a3b6d05b2bd9bdc908eb1eac0898d11
moff <moffmuriithi98@gmail.com>
2023-03-29 19:08:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for(let i=0; i<gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+      
+    }
+    return gifts;
+}
+const names = ["Ada", "Brendan", "Ali"];
+const event = "birthday";
+const messages = writeCards(names, event);
+
+function writeCards(names, event) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return messages;
+}
+
+//while loop
+
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  countDown(11);
